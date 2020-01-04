504643f467eda284a206f9020af6f1639629599c
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-04 22:02:35 +1100

    crazy refined version

--- src/com/codewardev/HumanReadableTime.java
+++ src/com/codewardev/HumanReadableTime.java
@@ -2,30 +2,12 @@ package com.codewardev;
 
 // https://www.codewars.com/kata/52685f7382004e774f0001f7/train/java
 
-import java.sql.Time;
-import java.time.Instant;
 
 public class HumanReadableTime {
 
 	public static String makeReadable(int seconds) {
-		
-		String[] divisor = {"10", "6", ":", "10", "6", ":", "10", "10"};
-		
-		Time a = new Time(seconds);
-		System.out.println("minute: "+a.getSeconds());
-		
-		String hrt = "";
-		for(int i=0; i<divisor.length; i++) {
-			try {
-				int x = Integer.valueOf(divisor[i]);
-				hrt = (seconds % x) + hrt;
-				seconds /= x;
-			} catch(NumberFormatException e) {
-				hrt = divisor[i]+hrt;
-			}
-		}
 
-		return hrt;
+		return String.format("%02d:%02d:%02d", (seconds/3600)%100, (seconds/60)%60, seconds%60);
 	}
 
 }
